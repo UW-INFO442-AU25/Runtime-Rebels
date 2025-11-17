b06c423b5c20c406ea36d5c47d12f41b66fd20c6
Yoonsoo Cho <ycho65@uw.edu>
2025-11-17 12:01:08 -0800

    added spacing

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -401,7 +401,7 @@ export default function Events() {
                 boxShadow: "var(--shadow-soft)",
               }}>
                 <p style={{ 
-                  margin: 0, 
+                  margin: "0 0 1.5rem 0", 
                   fontSize: "var(--text-size)", 
                   color: "var(--color-gray)",
                   fontFamily: "var(--font-family-base)"
